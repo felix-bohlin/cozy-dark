bc4e4556c69e3d8aff92a926500da09e7b8e8c1c
Felix <felix.bohlin@protonmail.com>
2023-11-20 10:58:13 +0100

    fix: wrong prop before

--- src/cozy-dark/cozy-dark.js
+++ src/cozy-dark/cozy-dark.js
@@ -103,6 +103,7 @@ export const cozyDark = {
     "input.background": editor.limedSpruce,
     "input.border": editor.limedSpruce,
     "input.foreground": common.gray[100],
+    "input.selectionBackground": common.primary.main,
     "input.placeholderForeground": common.gray[100],
     "inputOption.activeBorder": editor.dodgerBlue2,
     "inputOption.activeBackground": common.primary.main,
